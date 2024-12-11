56405ba58201edc5ebc735bdd564dff32401116d
Fanuel Couto <fanuel.couto99@gmail.com>
2024-12-11 17:30:59 -0300

    BUILD[App]: Criando projeto mobile

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+export default function Index() {
+    return (
+        <View>
+            <Text>
+                Vai Caraio
+            </Text>
+        </View>
+    );
+};
