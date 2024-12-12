4cfad605eb7088b6b4be25219bb35f18448d53a7
Fanuel Couto <fanuel.couto99@gmail.com>
2024-12-12 10:32:36 -0300

    FEAT[App]: Criar primeira tela da aplicação

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { colors } from '@/styles/theme';
 import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from '@expo-google-fonts/rubik';
+import { Loading } from "@/components/loading";
 
 export default function Layout() {
     const [fontsLoaded] = useFonts({
@@ -11,7 +12,7 @@ export default function Layout() {
     });
 
     if(!fontsLoaded) {
-        return;
+        return <Loading />;
     };
 
     return <Stack
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,11 +1,19 @@
-import {View, Text} from 'react-native';
+import { Button } from '@/components/button';
+import { Steps } from '@/components/steps';
+import { Welcome } from '@/components/welcome';
+import { View } from 'react-native';
 
 export default function Index() {
     return (
-        <View>
-            <Text>
-                Vai Caraio
-            </Text>
+        <View style={{ flex: 1, padding: 40, gap: 40 }}>
+            <Welcome />
+            <Steps />
+
+            <Button>
+                <Button.Title>
+                    Começar
+                </Button.Title>
+            </Button>
         </View>
     );
 };
